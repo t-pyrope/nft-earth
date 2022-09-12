26e85e2c06f20e502e7633a72acae43de9ca7088
Gomathi T <62291940+Gomathi1806@users.noreply.github.com>
2022-09-12 16:46:26 +0100

    Update vite.config.js

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,9 +1,22 @@
 import { defineConfig } from 'vite' ;
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 export default defineConfig({
+  
   root: 'src',
   build: {
     outDir: '../dist',
-    
   },
-});  
+  
+  plugins: [
+    createHtmlPlugin({
+      inject: {
+        data: {
+          title: env == '' '':' [${env.toUpperCase()}]',
+        },
+      },
+    }),
+   ],
+  
+  
+ });  
